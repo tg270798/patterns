0ac353b758ce085da520938d17d37b0837508b1f
Tathagat Sharma <49808976+tg270798@users.noreply.github.com>
2021-10-10 09:53:20 +0530

    Create pattern9.java

---
+++ pattern9.java
@@ -0,0 +1,26 @@
+
+public class Main
+{
+    static void pattern9(int n){
+        int k =0;
+        for(int row = 1;row<=n;row++){
+            if(k>0){
+                for(int space = 1;space<=k;space++){
+                    System.out.print(" ");
+                }
+            }
+            
+            
+            for(int star = (2*(n-row+1))-1;star>=1;star--){
+                System.out.print("*");
+            }
+            k++;
+            System.out.println();
+        }
+    }
+    
+	public static void main(String[] args) {
+		int n=5;
+		pattern9(n);
+	}
+}
